c501a05dfe7cadcf007af44f1e08c4719bce66ae
rafallmind <45233332+rafallmind@users.noreply.github.com>
2022-05-01 20:22:02 +0200

    Fix Package Test

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodoItem_Test.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodoItem_Test.java
@@ -1,4 +1,4 @@
-package com.acme.todolist.domain;
+package com.acme.todolist;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
